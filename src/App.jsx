1aadbbb31b4229748836e936e7c1c33ec66f331d
code-with-zain-hunzai <dev.zainulabbdin@gmail.com>
2025-03-27 13:07:07 +0500

    add map method for coreConcept array

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import CoreConcept from "./components/CoreConcept";
 import "./App.css";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Components");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selected) {
     setSelectedTopic(selected);
@@ -18,49 +18,59 @@ function App() {
         <Header />
         <main className="mt-6">
           <section className="text-center">
-            <h2 className="text-3xl font-semibold">
-              Core Concepts
-            </h2>
+            <h2 className="text-3xl font-semibold">Core Concepts</h2>
             <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mt-6">
-              <CoreConcept {...coreConcepts[0]} />
-              <CoreConcept {...coreConcepts[1]} />
-              <CoreConcept {...coreConcepts[2]} />
-              <CoreConcept {...coreConcepts[3]} />
+              {coreConcepts.map((conceptItem) => (
+                <CoreConcept key={conceptItem.title} {...conceptItem} />
+              ))}
             </ul>
           </section>
 
           <section className="mt-16 text-center">
             <h2 className="text-3xl font-semibold">Example</h2>
             <menu className="mt-6">
-              <div className="flex flex-wrap justify-center gap-4">
-                <TabButton onSelect={() => handleSelect("Components")}>
+              <div className="flex flex-wrap justify-center gap-4 pb-10">
+                <TabButton
+                  isSelect={selectedTopic === "Components"}
+                  onSelect={() => handleSelect("Components")}
+                >
                   Components
                 </TabButton>
-                <TabButton onSelect={() => handleSelect("Jsx")}>
+                <TabButton
+                  isSelect={selectedTopic === "Jsx"}
+                  onSelect={() => handleSelect("Jsx")}
+                >
                   Jsx
                 </TabButton>
-                <TabButton onSelect={() => handleSelect("State")}>
+                <TabButton
+                  isSelect={selectedTopic === "State"}
+                  onSelect={() => handleSelect("State")}
+                >
                   State
                 </TabButton>
-                <TabButton onSelect={() => handleSelect("Props")}>
+                <TabButton
+                  isSelect={selectedTopic === "Props"}
+                  onSelect={() => handleSelect("Props")}
+                >
                   Props
                 </TabButton>
-            
               </div>
-
-              <div className="mt-8 p-6 bg-white shadow-lg rounded-lg w-full  mx-auto">
-                <h3 className="text-2xl font-semibold text-gray-700">
-                  {Examples[selectedTopic]?.title}
-                </h3>
-                <p className="text-gray-600 mt-2">
-                  {Examples[selectedTopic]?.description}
-                </p>
-                <pre className="bg-gray-900 text-white p-4 rounded-md mt-4 text-left overflow-x-auto">
-                  <code className="text-sm">
-                    {Examples[selectedTopic]?.code}
-                  </code>
-                </pre>
-              </div>
+              {!selectedTopic && <p>Please select a topic</p>}
+              {selectedTopic && (
+                <div className="mt-8 p-6 bg-white shadow-lg rounded-lg w-full  mx-auto">
+                  <h3 className="text-2xl font-semibold text-gray-700">
+                    {Examples[selectedTopic]?.title}
+                  </h3>
+                  <p className="text-gray-600 mt-2">
+                    {Examples[selectedTopic]?.description}
+                  </p>
+                  <pre className="bg-gray-900 text-white p-4 rounded-md mt-4 text-left overflow-x-auto">
+                    <code className="text-sm">
+                      {Examples[selectedTopic]?.code}
+                    </code>
+                  </pre>
+                </div>
+              )}
             </menu>
           </section>
         </main>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,8 +1,14 @@
-export default function TabButton({ children, onSelect }) {
-    return (
-      <li>
-        <button onClick={onSelect}>{children}</button>
-      </li>
-    );
-  }
-  
+export default function TabButton({ children, onSelect, isSelect }) {
+  return (
+    <li>
+      <button
+        className={`px-4 py-2 rounded-md transition-all ${
+          isSelect ? "bg-blue-500 text-white" : undefined
+        }`}
+        onClick={onSelect}
+      >
+        {children}
+      </button>
+    </li>
+  );
+}
